76f2f00dd8067a774da9cbe1e3a8205b78a861d4
songjeonghwa <sjhw7200@naver.com>
2026-04-02 00:40:46 +0900

    feat/signup

---
+++ src/api/auth.api.ts
@@ -0,0 +1,8 @@
+import { Sign } from "crypto";
+import { httpClient } from "./http";
+import { SignupProps } from "../pages/Signup";
+
+export const signup = async(userData: SignupProps) => {
+  const response = await httpClient.post("/users/join", userData);
+  return response.data;
+}
